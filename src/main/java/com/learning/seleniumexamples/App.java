ff119c08feed657ced650ce85ed799415f079e5a
Swaroop Kumar Tata <swaroop.kumar.tata@oracle.com>
2018-08-30 12:08:08 -0400

    Firsts Commit

---
+++ src/main/java/com/learning/seleniumexamples/App.java
@@ -0,0 +1,13 @@
+package com.learning.seleniumexamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
